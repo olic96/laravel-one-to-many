93704c98ce1b31809152d267442ae8d264acf108
olic96 <francoolich@gmail.com>
2022-07-21 16:15:45 +0200

    add crud

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware('auth')
     ->group(function() {
      Route::get('/home', 'HomeController@index')->name('home');
      Route::resource('posts', 'PostController');
+     Route::resource('categories', 'CategoryController');
     });
     
 // front office
